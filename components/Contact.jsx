ab13e6a29dd732aa06372a602a449e8a50a97a70
shubham <shubhamrawatktw@gmail.com>
2024-04-03 12:14:54 +0530

    changes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -11,46 +11,29 @@ const Contact = () => {
   return (
     <div id="contact" className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full ">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Contact
-        </p>
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">Contact</p>
         <h2 className="py-4 ">Get In Touch</h2>
         <div className="grid lg:grid-cols-5 gap-8">
           {/* left */}
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full ">
               <div>
-                <Image
-                  className="rounded-xl hover:scale-105 ease-in duration-300"
-                  src={ContactImg}
-                  alt="/"
-                />
+                <Image className="rounded-xl hover:scale-105 ease-in duration-300" src={ContactImg} alt="/" />
               </div>
               <div>
                 <h2 className="py-2">Sourav Rawat</h2>
                 <p>Front-End Developer</p>
-                <p className="py-4">
-                  I am available for freelance or full-time positions. Contact
-                  me and let&apos;s talk.
-                </p>
+                <p className="py-4">I am available for freelance or full-time positions. Contact me and let&apos;s talk.</p>
               </div>
               <div>
                 <p className="uppercase pt-8">Connect With Me</p>
                 <div className="flex items-center justify-between py-4">
-                  <a
-                    href="https://www.linkedin.com/in/clint-briley-50056920a/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                  <a href="https://www.linkedin.com/in/sourav-rawat-0410ba275/" target="_blank" rel="noreferrer">
                     <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                       <FaLinkedinIn />
                     </div>
                   </a>
-                  <a
-                    href="https://github.com/fireclint"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
+                  <a href="https://github.com/Sourabhrwt" target="_blank" rel="noreferrer">
                     <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                       <FaGithub />
                     </div>
@@ -60,11 +43,9 @@ const Contact = () => {
                     <AiOutlineMail />
                   </div>
                   <Link href="/resume">
-                 
-                      <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-                        <BsFillPersonLinesFill />
-                      </div>
-                   
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                      <BsFillPersonLinesFill />
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -74,58 +55,30 @@ const Contact = () => {
           {/* right */}
           <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <div className="p-4">
-              <form
-                action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060"
-                method="POST"
-                encType="multipart/form-data"
-              >
+              <form action="https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060" method="POST" encType="multipart/form-data">
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
-                    <input
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
-                      type="text"
-                      name="name"
-                    />
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="name" />
                   </div>
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
-                      Phone Number
-                    </label>
-                    <input
-                      className="border-2 rounded-lg p-3 flex border-gray-300"
-                      type="text"
-                      name="phone"
-                    />
+                    <label className="uppercase text-sm py-2">Phone Number</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="phone" />
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Email</label>
-                  <input
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="email"
-                    name="email"
-                  />
+                  <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email" />
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Subject</label>
-                  <input
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="text"
-                    name="subject"
-                  />
+                  <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject" />
                 </div>
                 <div className="flex flex-col py-2">
                   <label className="uppercase text-sm py-2">Message</label>
-                  <textarea
-                    className="border-2 rounded-lg p-3 border-gray-300"
-                    rows="4"
-                    name="message"
-                  ></textarea>
+                  <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="4" name="message"></textarea>
                 </div>
-                <button className="w-full p-4 text-gray-100 mt-4">
-                  Send Message
-                </button>
+                <button className="w-full p-4 text-gray-100 mt-4">Send Message</button>
               </form>
             </div>
           </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -9,32 +9,19 @@ const Main = () => {
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET&#39;S BUILD SOMETHING TOGETHER
-          </p>
+          <p className="uppercase text-sm tracking-widest text-gray-600">LET&#39;S BUILD SOMETHING TOGETHER</p>
           <h1 className="py-4 text-gray-700">
             Hi, I&#39;m <span className="text-[#5651e5]">Sourav</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
-          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
-            I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
-          </p>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">I’m focused on building responsive front-end web applications integrating back-end technologies.</p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <a
-              href="https://www.linkedin.com/in/clint-briley-50056920a/"
-              target="_blank"
-              rel="noreferrer"
-            >
+            <a href="https://www.linkedin.com/in/sourav-rawat-0410ba275/" target="_blank" rel="noreferrer">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <FaLinkedinIn />
               </div>
             </a>
-            <a
-              href="https://github.com/Sourabhrwt"
-              target="_blank"
-              rel="noreferrer"
-            >
+            <a href="https://github.com/Sourabhrwt" target="_blank" rel="noreferrer">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <FaGithub />
               </div>
